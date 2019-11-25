881d1d0b1c1db2b3623e82851438cb01a21cf155
Andrew Pattison <58046090+andrum99@users.noreply.github.com>
2019-11-25 22:41:56 +0000

    Update build32.sh

--- build32.sh
+++ build32.sh
@@ -20,7 +20,7 @@ sudo modprobe zfs
 sudo apt install raspberrypi-kernel-headers
 
 # https://github.com/zfsonlinux/zfs/wiki/Building-ZFS
-sudo apt install -y autoconf automake libtool gawk alien fakeroot ksh
+sudo apt install -y autoconf automake libtool gawk alien fakeroot ksh libssl-dev
 sudo apt install -y zlib1g-dev uuid-dev libattr1-dev libblkid-dev libselinux-dev libudev-dev
 sudo apt install -y libacl1-dev libaio-dev libdevmapper-dev libelf-dev
 sudo apt install -y python3 python3-dev python3-setuptools python3-cffi
@@ -32,5 +32,5 @@ make distclean || true
 autoreconf --install --force
 ./configure
 
-make -s -j$(nproc)
+make -s -j6
 sudo make install
