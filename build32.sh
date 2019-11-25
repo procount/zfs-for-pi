2f9338488504de49a630955958d55ee73ccbaa9b
Andrew Pattison <58046090+andrum99@users.noreply.github.com>
2019-11-25 22:19:15 +0000

    Update build32.sh

--- build32.sh
+++ build32.sh
@@ -17,3 +17,20 @@ sudo modprobe zfs
 
 # build 32-bit part of zfs-linux
 
+sudo apt install raspberrypi-kernel-headers
+
+# https://github.com/zfsonlinux/zfs/wiki/Building-ZFS
+sudo apt install -y autoconf automake libtool gawk alien fakeroot ksh
+sudo apt install -y zlib1g-dev uuid-dev libattr1-dev libblkid-dev libselinux-dev libudev-dev
+sudo apt install -y libacl1-dev libaio-dev libdevmapper-dev libelf-dev
+sudo apt install -y python3 python3-dev python3-setuptools python3-cffi
+
+make clean || true
+make distclean || true
+
+./autogen.sh
+autoreconf --install --force
+./configure --with-linux=/home/pi/linux-raspberrypi-kernel_1.20190925-1
+
+make -s -j$(nproc)
+sudo make install
