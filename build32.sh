de1f5931e2b95ac34b02e5660dd4e69d103fa78a
Andrew Pattison <58046090+andrum99@users.noreply.github.com>
2019-11-25 21:19:19 +0000

    Create build32.sh

---
+++ build32.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# Install 64-bit kernel modules for Raspbian, and build 32-bit userland
+# by andrum99
+
+# This script must be run under a 32-bit userland on a 64-bit kernel
+
+# Copy 64-bit kernel modules from where build64 left them
+mkdir mods
+cd mods
+tar xvf 64-bit-zfs-modules.tar.gz
+cp -a * /lib/modules/4.19.75-v8+
+
+# Load the modules
+sudo ldconfig
+sudo depmod -a
+sudo modprobe zfs
+sudo zpool status
