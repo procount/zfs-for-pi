92e8128181b8a5c7acaf14c67e407e25546986de
Andrew Pattison <58046090+andrum99@users.noreply.github.com>
2019-11-25 22:45:33 +0000

    Update build32.sh

--- build32.sh
+++ build32.sh
@@ -2,6 +2,14 @@
 # Install 64-bit kernel modules for Raspbian, and build 32-bit userland
 # by andrum99
 
+# This script must be run under a 32-bit userland on a 64-bit kernel
+
+# Copy 64-bit kernel modules from where build64.sh left them
+mkdir mods
+cd mods
+tar xvf ../64-bit-zfs-modules.tar.gz
+cp -a * /lib/modules/4.19.75-v8+
+
 # build 32-bit part of zfs-linux
 
 sudo apt install raspberrypi-kernel-headers
