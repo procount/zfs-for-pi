55dc5fb47a50523a284af3d340d2845e4922ebec
Andrew Pattison <58046090+andrum99@users.noreply.github.com>
2019-11-25 22:42:33 +0000

    Update build64.sh

--- build64.sh
+++ build64.sh
@@ -34,13 +34,13 @@ make modules -j6
 
 if [ ! -d zfs ]; then
     # https://github.com/zfsonlinux/zfs/wiki/Building-ZFS
-    sudo apt-get install -y autoconf automake libtool gawk alien fakeroot ksh
-    sudo apt-get install -y zlib1g-dev uuid-dev libattr1-dev libblkid-dev libselinux-dev libudev-dev
-    sudo apt-get install -y libacl1-dev libaio-dev libdevmapper-dev libelf-dev
-    sudo apt-get install -y python3 python3-dev python3-setuptools python3-cffi
+    sudo apt install -y autoconf automake libtool gawk alien fakeroot ksh
+    sudo apt install -y zlib1g-dev uuid-dev libattr1-dev libblkid-dev libselinux-dev libudev-dev
+    sudo apt install -y libacl1-dev libaio-dev libdevmapper-dev libelf-dev
+    sudo apt install -y python3 python3-dev python3-setuptools python3-cffi
 
-    #sudo apt-get install -y linux-headers-$(uname -r)
-    #sudo apt-get install raspberrypi-kernel-headers
+    #sudo apt install -y linux-headers-$(uname -r)
+    #sudo apt install raspberrypi-kernel-headers
 
     git clone https://github.com/zfsonlinux/zfs.git
 fi
@@ -58,7 +58,7 @@ make distclean || true
 autoreconf --install --force
 ./configure --with-linux=/home/pi/linux-raspberrypi-kernel_1.20190925-1
 
-make -s -j$(nproc)
+make -s -j6
 sudo make install
 
 tar -cvzf 64-bit-zfs-modules.tar.gz /lib/modules/4.19.75-v8/
