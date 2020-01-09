898f5eec4a84f405c5b7d49941ab8dec259516f6
Andrew Pattison <58046090+andrum99@users.noreply.github.com>
2020-01-09 12:32:33 +0000

    Fix download of kernel source
    See #1

--- build64.sh
+++ build64.sh
@@ -22,7 +22,7 @@ sudo apt install build-essential bison flex bc libssl-dev wget git
 # Build Linux kernel. Most of this we don't use - we just need the
 # kernel headers and the Module.symvers
 
-git clone https://github.com/raspberrypi/linux/archive/raspberrypi-kernel_1.20190925-1.tar.gz
+wget https://github.com/raspberrypi/linux/archive/raspberrypi-kernel_1.20190925-1.tar.gz
 cd linux-raspberrypi-kernel_1.20190925-1
 KERNEL=kernel8
 make bcm2711_defconfig
