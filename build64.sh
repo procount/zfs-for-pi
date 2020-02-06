dacbe1c5bb8713db464708e23a740704e45056a9
Andrew Pattison <58046090+andrum99@users.noreply.github.com>
2020-02-06 21:20:30 +0000

    Update build64.sh

--- build64.sh
+++ build64.sh
@@ -20,9 +20,9 @@ sudo apt install -y build-essential bison flex bc libssl-dev wget git
 # Build Linux kernel. Most of this we don't use - we just need the
 # kernel headers and the Module.symvers
 
-wget https://github.com/raspberrypi/linux/archive/raspberrypi-kernel_1.20200114-1.tar.gz
-tar -xvf raspberrypi-kernel_1.20200114-1.tar.gz
-cd linux-raspberrypi-kernel_1.20200114-1
+wget https://github.com/raspberrypi/linux/archive/raspberrypi-kernel_1.20200205-1.tar.gz
+tar -xvf raspberrypi-kernel_1.20200205-1.tar.gz
+cd linux-raspberrypi-kernel_1.20200205-1
 KERNEL=kernel8
 make bcm2711_defconfig
 make -j6
@@ -55,13 +55,13 @@ make distclean || true
 
 ./autogen.sh
 autoreconf --install --force
-./configure --with-linux=/home/pi/linux-raspberrypi-kernel_1.20200114-1
+./configure --with-linux=/home/pi/linux-raspberrypi-kernel_1.20200205-1
 
 make -s -j6
 sudo make install
 
-sudo mv /lib/modules/4.19.93-v8 /lib/modules/4.19.93-v8+
-tar -cvzf /home/pi/64-bit-zfs-modules-4.19.93.tar.gz /lib/modules/4.19.93-v8+/extra
+sudo mv /lib/modules/4.19.93-v8 /lib/modules/4.19.97-v8+
+tar -cvzf /home/pi/64-bit-zfs-modules-4.19.93.tar.gz /lib/modules/4.19.97-v8+/extra
 echo "Now exit the 64-bit userland, back to 32-bit userland (staying on 64-bit kernel) and run build64_part2.sh"
 
 cd "${CUR_DIR}"
