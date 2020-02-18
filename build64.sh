6fd199c79a4b1cf2120afe661de81a35f1534901
procount <kevin.procount@googlemail.com>
2020-02-18 15:00:51 +0000

    Changed KERNEL -> KVERSION

--- build64.sh
+++ build64.sh
@@ -61,7 +61,7 @@ autoreconf --install --force
 make -s -j6
 sudo make install
 
-tar -cvzf /home/pi/64-bit-zfs-modules-${KERNEL}-v8+.tar.gz /lib/modules/${KERNEL}-v8/extra
+tar -cvzf /home/pi/64-bit-zfs-modules-${KVERSION}-v8+.tar.gz /lib/modules/${KVERSION}-v8/extra
 echo "Now exit the 64-bit userland, back to 32-bit userland (staying on 64-bit kernel) and run build64_part2.sh"
 
 cd "${CUR_DIR}"
